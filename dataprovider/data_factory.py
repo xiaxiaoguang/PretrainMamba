2c6075afd0858362ada16d91bb9740f4cccfe2c9
929897735 <52369899+929897735@users.noreply.github.com>
2024-10-14 13:39:21 +0800

    Add files via upload

---
+++ dataprovider/data_factory.py
@@ -0,0 +1,62 @@
+import sys
+sys.path.append('/home/srwang/survey/dataprovider')
+from data_loader import Dataset_Traffic, Dataset_KDDcup, Dataset_Finance, Dataset_Solar,Dataset_ETT_minute,Dataset_ETT_hour
+from torch.utils.data import DataLoader
+import pandas as pd
+
+data_dict = {
+    'traffic': Dataset_Solar,
+    'electricity':Dataset_Solar,
+    'ETTh1': Dataset_ETT_hour,
+    'ETTh2': Dataset_ETT_hour,
+    'ETTm1': Dataset_ETT_minute,
+    'ETTm2': Dataset_ETT_minute,
+}
+
+
+def data_provider(args, flag):
+    Data = data_dict[args.data]
+
+    if flag == 'test' or flag=='val':
+        shuffle_flag = False
+        drop_last = False
+        batch_size = args.batch_size
+
+    else:
+        shuffle_flag = True
+        drop_last = True
+        batch_size = args.batch_size
+
+    if args.data == 'kdd17' or args.data == 'acl18':
+        data_set = Data(
+            root_path=args.root_path,
+            data_path=args.data_path,
+            flag=flag,
+            size=[args.seq_len, args.pred_len],
+            data_name=args.data,
+            stock_num=args.num_nodes,
+        )
+    else:
+        data_set = Data(
+            root_path=args.root_path,
+            data_path=args.data_path,
+            flag=flag,
+            size=[args.seq_len, args.pred_len],
+            device=args.device
+        )
+    print(flag, len(data_set))
+    data_loader = DataLoader(
+        data_set,
+        batch_size=batch_size,
+        shuffle=shuffle_flag,
+        num_workers=args.num_workers,
+        drop_last=drop_last)
+    return data_set, data_loader
+
+def load_adj(data, adj_path):
+    raw_adj = pd.read_pickle(adj_path)
+    if data == 'METR-LA' or data == 'PEMS-BAY':
+        adj = raw_adj[2]
+    else:
+        adj = raw_adj
+    return adj
